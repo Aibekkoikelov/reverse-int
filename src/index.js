d202688e979559918e97779e0bafb66c37ac164b
Aibek <aibekkoikelov@gmail.com>
2022-01-03 13:25:55 +0600

    feat: create function for reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+function reverse(n) {
+  n = n < 0 ? n * -1 : n;
+  return +n.toString().split("").reverse().join("");
+   
 }
+module.exports = reverse;
+
+
+
+console.log(Math.ceil());
+console.log(1700 / 100);
+
+
+
+
+
+
+
